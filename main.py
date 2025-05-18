4bdf6afea9188a5a00e88f7f3a3aa1ada62f3a49
molsche <molsche@gmx.de>
2025-05-18 10:06:10 +0200

    All

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+from dino_test import play_dino
+from identify_window_with_mouse_location import get_mouse_location
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    play_dino()
+    # get_mouse_location()
